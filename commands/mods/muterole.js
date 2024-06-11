cd486e0b86d689e310919b6b77de77c75a644dbc
Hydroxho <hydrobtw1@gmail.com>
2024-06-11 16:59:08 +0200

    Update muterole.js

--- commands/mods/muterole.js
+++ commands/mods/muterole.js
@@ -10,72 +10,146 @@ const {
 	MessageEmbed
 } = require("discord.js")
 
-module.exports = {
-	name: 'muterole',
-	aliases: [],
+function sleep(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms)
+	})
+}
 
+module.exports = {
+	name: 'mutelist',
+	aliases: [],
 	run: async (client, message, args, prefix, color) => {
+
 		let perm = ""
 		message.member.roles.cache.forEach(role => {
-			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = null
-			if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = null
+
+			if (db.get(`modsp_${message.guild.id}_${role.id}`)) perm = true
+			if (db.get(`admin_${message.guild.id}_${role.id}`)) perm = true
+			if (db.get(`ownerp_${message.guild.id}_${role.id}`)) perm = true
 		})
 		if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {
 			let Muted = await db.fetch(`mRole_${message.guild.id}`);
+			let muteRole = await message.guild.roles.cache.get(Muted) || message.guild.roles.cache.find(role => role.name === `muet`) || message.guild.roles.cache.find(role => role.name === `Muted`) || message.guild.roles.cache.find(role => role.name === `Mute`)
 
-			let muterole = await message.guild.roles.cache.get(Muted) || message.guild.roles.cache.find(role => role.name === `muet`) || message.guild.roles.cache.find(role => role.name === `Muted`) || message.guild.roles.cache.find(role => role.name === `Mute`)
-			if (muterole) {
-				const embed = new Discord.MessageEmbed()
-				embed.setColor(color)
-				embed.setFooter(`${client.config.name}`)
-				embed.setDescription(`**Il existe déjà un rôle muet : <@&${muterole.id}>**\nVérification des permissions du rôles muet en cours`)
-				message.channel.send(embed).then(async mm => {
-					const embed2 = new Discord.MessageEmbed()
-					embed2.setTitle("Les permissions du rôle muet ont été mises à jour")
-					embed2.setColor(color)
-					embed2.setFooter(`${client.config.name}`)
+			if (!muteRole) return message.channel.send(new Discord.MessageEmbed().setColor(color).setTitle("Aucun mute en cours"))
+			if (muteRole.members.size === 0 || undefined || false || null) return message.channel.send(new Discord.MessageEmbed().setColor(color).setFooter(`${client.config.name}`).setTitle("Aucun mute en cours"))
 
-					const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
-					channels.forEach(channel => {
+			let p0 = 0;
+			let p1 = 5;
+			let page = 1;
+
+			const embed = new Discord.MessageEmbed()
+				.setTitle('Mute en cours')
+				.setDescription(muteRole.members
+					.map(r => r)
+					.filter(x => message.guild.members.cache.get(x.user.id))
+					.map((m, i) => `${i+1}) ${m.user} (${m.user.id})`)
+					.slice(0, 5)
+				)
+
+				.setColor(color)
+				.setFooter(`Total: ${muteRole.members.size} • ${client.config.name}`)
 
 
-						channel.createOverwrite(muterole, {
-							SEND_MESSAGES: false,
-							CONNECT: false,
-							ADD_REACTIONS: false
-						}, "Muterole")
-						embed2.setDescription(`**__D'autres permission déjà existantes peuvent rendre innefficace le mute pour certains rôles dans les salons suivants :__**\n\n**${channel.name}**\n- ${muterole.name}\n`, true)
-						embed2.setFooter(`${client.config.name} Tous les rôles ayant la permissons \"envoyer des messages\" en vert seront insensible au mute`)
+			message.channel.send(embed).then(async tdata => {
+				if (muteRole.members.size > 5) {
+					const B1 = new MessageButton()
+						.setLabel("◀")
+						.setStyle("gray")
+						.setID('mutelist1');
 
+					const B2 = new MessageButton()
+						.setLabel("▶")
+						.setStyle("gray")
+						.setID('mutelist2');
+
+					const bts = new MessageActionRow()
+						.addComponent(B1)
+						.addComponent(B2)
+					tdata.edit("", {
+						embed: embed,
+						components: [bts]
 					})
+					setTimeout(() => {
+						tdata.edit("", {
+							components: [],
+							embed: new Discord.MessageEmbed()
+								.setTitle('Mute en cours')
+								.setDescription(muteRole.members
+									.map(r => r)
+									.filter(x => message.guild.members.cache.get(x.user.id))
+									.map((m, i) => `${i+1}) ${m.user} (${m.user.id})`)
+									.slice(0, 5)
+								)
 
-					message.channel.send(embed2)
-				})
-				return;
-			}
-			if (!muterole) {
-				const embed = new Discord.MessageEmbed()
-				embed.setColor(color)
-				embed.setTitle(`Création d'un rôle muet`)
-				message.channel.send(embed).then(async m => {
-					muterole = await message.guild.roles.create({
-						data: {
-							name: 'muet',
-							permissions: 0
+								.setColor(color)
+								.setFooter(`Total: ${muteRole.members.size} • ${client.config.name}`)
+
+
+						})
+						// message.channel.send(embeds)
+					}, 60000 * 5)
+					client.on("clickButton", (button) => {
+						if (button.clicker.user.id !== message.author.id) return;
+						if (button.id === "mutelist1") {
+							button.reply.defer(true)
+
+							p0 = p0 - 5;
+							p1 = p1 - 5;
+							page = page - 1
+
+							if (p0 < 0) {
+								return
+							}
+							if (p0 === undefined || p1 === undefined) {
+								return
+							}
+
+							embed.setDescription(muteRole.members
+									.map(r => r)
+									.filter(x => message.guild.members.cache.get(x.user.id))
+									.map((m, i) => `${i+1}) ${m.user} (${m.user.id})`)
+									.slice(p0, p1)
+								)
+
+								.setColor(color)
+								.setFooter(`Total: ${muteRole.members.size} • ${client.config.name}`)
+							tdata.edit(embed);
+
+						}
+						if (button.id === "mutelist2") {
+							button.reply.defer(true)
+
+							p0 = p0 + 5;
+							p1 = p1 + 5;
+
+							page++;
+
+							if (p1 > muteRole.members.size + 5) {
+								return
+							}
+							if (p0 === undefined || p1 === undefined) {
+								return
+							}
+
+							embed.setDescription(muteRole.members
+									.map(r => r)
+									.filter(x => message.guild.members.cache.get(x.user.id))
+									.map((m, i) => `${i+1}) ${m.user} (${m.user.id})`)
+									.slice(p0, p1)
+								)
+
+								.setColor(color)
+								.setFooter(`Total: ${muteRole.members.size} • ${client.config.name}`)
+							tdata.edit(embed);
+
 						}
 					})
-					message.guild.channels.cache.forEach(channel => channel.createOverwrite(muterole, {
-						SEND_MESSAGES: false,
-						CONNECT: false,
-						ADD_REACTIONS: false
-					}, "Muterole"))
-					db.set(`mRole_${message.guild.id}`, `${muterole.id}`)
-					const e = new Discord.MessageEmbed()
-					e.setColor(color)
-					e.setDescription(`***Rôle muet créé :*** ${muterole}`)
-					return m.edit("", e)
-				})
-			}
+				}
+
+			})
+
 		}
 	}
 }
