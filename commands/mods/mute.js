2ba797ddb05576937a945ba5383993f1b57c76e6
Hydroxho <hydrobtw1@gmail.com>
2024-06-11 15:52:16 +0200

    mute.js

--- commands/mods/mute.js
+++ commands/mods/mute.js
@@ -15,20 +15,19 @@ module.exports = {
         });
         if (client.config.owner.includes(message.author.id) || db.get(`ownermd_${client.user.id}_${message.author.id}`) === true || perm) {
 
-            function mute(user, authorcooldown, muterole, time = null) {
+            async function mute(user, authorcooldown, muterole, time = null) {
                 db.set(`mute_${message.guild.id}_${user.id}`, true);
-                user.roles.add(muterole.id).then(r => {
-                    authorcooldown.limit++;
-                    if (time) {
-                        setTimeout(() => {
-                            user.roles.remove(muterole.id);
-                            db.set(`mute_${message.guild.id}_${user.id}`, null);
-                        }, time);
-                    }
-                    setTimeout(() => {
-                        authorcooldown.limit--;
-                    }, 120000);
-                });
+                await user.roles.add(muterole.id);
+                authorcooldown.limit++;
+                if (time) {
+                    setTimeout(async () => {
+                        await user.roles.remove(muterole.id);
+                        db.set(`mute_${message.guild.id}_${user.id}`, null);
+                    }, time);
+                }
+                setTimeout(() => {
+                    authorcooldown.limit--;
+                }, 120000);
             }
 
             if (args[0]) {
@@ -36,7 +35,10 @@ module.exports = {
                 const logsmod = message.guild.channels.cache.get(chx);
 
                 let Muted = await db.fetch(`mRole_${message.guild.id}`);
-                let muterole = await message.guild.roles.cache.get(Muted) || message.guild.roles.cache.find(role => role.name === `muet`) || message.guild.roles.cache.find(role => role.name === `Muted`) || message.guild.roles.cache.find(role => role.name === `Mute`);
+                let muterole = message.guild.roles.cache.get(Muted) || 
+                               message.guild.roles.cache.find(role => role.name === `muet`) || 
+                               message.guild.roles.cache.find(role => role.name === `Muted`) || 
+                               message.guild.roles.cache.find(role => role.name === `Mute`);
 
                 var user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
                 if (!user) return message.channel.send(`Aucun membre trouvé pour: \`${args[0]}\``);
@@ -53,6 +55,7 @@ module.exports = {
                 var authorcooldown = cooldown[message.author.id];
 
                 if (authorcooldown.limit >= 5) return message.channel.send(`Vous avez atteint votre limite de mute, veuillez retenter plus tard!`);
+
                 if (!muterole) {
                     message.channel.send("Création d'un rôle muet...");
                     muterole = await message.guild.roles.create({
@@ -61,16 +64,22 @@ module.exports = {
                             permissions: []
                         }
                     }, "Muterole");
-                    message.guild.channels.cache.forEach(channel => channel.updateOverwrite(muterole, {
-                        SEND_MESSAGES: false,
-                        CONNECT: false,
-                        ADD_REACTIONS: false
-                    }, "Muterole"));
+
+                   
+                    message.guild.channels.cache.forEach(channel => {
+                        channel.updateOverwrite(muterole, {
+                            SEND_MESSAGES: false,
+                            CONNECT: false,
+                            ADD_REACTIONS: false,
+                            SPEAK: false
+                        }, "Muterole");
+                    });
+
                     db.set(`mRole_${message.guild.id}`, `${muterole.id}`);
                 }
 
                 let muteTime = args[1] ? ms(args[1]) : null;
-                mute(user, authorcooldown, muterole, muteTime);
+                await mute(user, authorcooldown, muterole, muteTime);
 
                 message.channel.send(`<@${user.id}> a été mute ${muteTime ? `pendant ${args[1]}` : 'indéfiniment'}.`);
                 if (logsmod) logsmod.send(`**${message.author.tag}** a mute **${user.user.tag}** ${muteTime ? `pendant ${args[1]}` : 'indéfiniment'}`);
